bd76a5f42cc06f039e8251d87f4f610f749b900b
mmuffins <mail@tiv73.com>
2017-07-13 16:54:55 +0200

    added viewhandlers

---
+++ viewHandlers_add.js
@@ -0,0 +1,20 @@
+var nowPlayingData = {
+  getTracklist: function() {
+    return app.player.getSongList().getTracklist()
+  }
+}
+
+nodeHandlers.root.override({
+  getChildren: function($super, node) {
+    return new Promise(function(resolve, reject) {
+      $super(node).then(function() {
+        node.addChild(nowPlayingData, 'npview')
+        resolve()
+      })
+    })
+  }
+})
+
+nodeHandlers.npview.override({
+  viewAs: ['tracklist', 'nowplaying']
+})
